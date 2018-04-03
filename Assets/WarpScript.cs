f21070c70a7e2bdd3f7d87590bd715e94a3773e1
syoya00 <tkd0617f@gmail.com>
2018-04-03 20:23:16 +0900

    change

---
+++ Assets/WarpScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class WarpScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Alpha0))
+        {
+            SceneManager.LoadScene("main0");
+        }
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("main1");
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("main2");
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("main3");
+        }
+
+    }
+}
